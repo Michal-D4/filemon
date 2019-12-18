475244d1663a76e8845030bcc2a2a1773d75aeec
Davidovich_M <mihal.d44@gmail.com>
2019-12-18 15:32:49 +0300

    move fixtures from conftest to test_create_db

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,10 +1,4 @@
-import sqlite3
-import pytest
 from pathlib import Path
-from typing import Iterable
-
-import src.core.create_db as db
-
 
 PATH_TO_DATA = Path('data')
 
@@ -23,53 +17,3 @@ FILES = [('Dirs.txt', 1),
          ]
 
 
-def create_sql(table: str, header: Iterable):
-    placeholders = ','.join(['?'] * len(header))
-    sql = f'INSERT INTO {table} ({",".join(header)}) VALUES ({placeholders});'
-    return sql
-
-
-@pytest.fixture()
-def init_db() -> sqlite3.Connection:
-    conn = sqlite3.connect(":memory:")
-    db.create_all_objects(conn)
-    for fl in FILES:
-        fll = PATH_TO_DATA / fl[0]
-        # if fl[0].startswith('inDirs'):
-        #     tbl = 'Dirs'
-        # else:
-        tbl = fl[0].split('.')[0]
-        vals = []
-        sql = ''
-        with open(fll) as fi:
-            is_header = True
-            for line in fi:
-                fields = line.strip('\n').split('|')[fl[1]:]
-                if is_header:
-                    sql = create_sql(tbl, fields)
-                    is_header = False
-                else:
-                    vals.append(tuple(fields))
-        if vals:
-            conn.executemany(sql, vals)
-
-    conn.execute('PRAGMA foreign_keys = ON;')
-    return conn
-
-
-@pytest.fixture()
-def expected() -> dict():
-    res = dict()
-    for fl in FILES:
-        fll = PATH_TO_DATA / fl[0]
-        vals = []
-        with open(fll) as fi:
-            skip_line = True
-            for line in fi:
-                if skip_line:
-                    skip_line = False
-                else:
-                    vals.append(tuple(line.strip('\n').split('|')))
-
-        res[fl[0].split('.')[0]] = vals
-    return res
--- src/test/test_create_db.py
+++ src/test/test_create_db.py
@@ -1,6 +1,8 @@
+from typing import Iterable
 import pytest
 import sqlite3
-import src.core.create_db as db
+from src.core import create_db as db
+from src.test.conftest import FILES, PATH_TO_DATA
 
 
 @pytest.fixture()
@@ -58,3 +60,53 @@ def test_load_data(init_db, expected):
                 if None not in row:
                     assert tuple(map(str,row)) == it[i]
                     i += 1
+
+
+@pytest.fixture()
+def expected() -> dict():
+    res = dict()
+    for fl in FILES:
+        fll = PATH_TO_DATA / fl[0]
+        values = []
+        with open(fll) as fi:
+            skip_line = True
+            for line in fi:
+                if skip_line:
+                    skip_line = False
+                else:
+                    values.append(tuple(line.strip('\n').split('|')))
+
+        res[fl[0].split('.')[0]] = values
+    return res
+
+
+@pytest.fixture()
+def init_db() -> sqlite3.Connection:
+    def create_sql(table: str, header: Iterable):
+        placeholders = ','.join(['?'] * len(header))
+        return f'INSERT INTO {table} ({",".join(header)}) VALUES ({placeholders});'
+
+    conn = sqlite3.connect(":memory:")
+    db.create_all_objects(conn)
+    for fl in FILES:
+        fll = PATH_TO_DATA / fl[0]
+        # if fl[0].startswith('inDirs'):
+        #     tbl = 'Dirs'
+        # else:
+        tbl = fl[0].split('.')[0]
+        vals = []
+        sql = ''
+        with open(fll) as fi:
+            is_header = True
+            for line in fi:
+                fields = line.strip('\n').split('|')[fl[1]:]
+                if is_header:
+                    sql = create_sql(tbl, fields)
+                    is_header = False
+                else:
+                    vals.append(tuple(fields))
+        if vals:
+            conn.executemany(sql, vals)
+
+    conn.execute('PRAGMA foreign_keys = ON;')
+    return conn
