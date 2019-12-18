fbce41d6b00b1de38f9c95debc1128c586f9f368
“Michal-D4” <mihal.d44@gmail.com>
2019-12-18 08:36:54 +0300

    params test yield_files function; failed

--- src/test/test_load_db_data.py
+++ src/test/test_load_db_data.py
@@ -6,19 +6,35 @@ PATH_TO_DATA = Path('data')
 ROOT = Path.cwd().parent.parent / 'test_data'
 
 
-@pytest.fixture()
-def expected_files():
+@pytest.fixture(params=[
+    ('',),
+    ('pdf', 'ui'),
+    '*'
+])
+def expected_files(request):
+    def check_ext(ext, file_parts: tuple) -> bool:
+        if '*' in ext:
+            return True
+        last_part = file_parts[-1]
+        name, ext_ = last_part.rpartition('.')[0::2]
+        file_ext = ext_ if name else ''
+        return file_ext in ext
+
+    ext = request.param
     files = []
     with open(PATH_TO_DATA / 'file_list.txt') as fl:
         for line in fl:
-            files.append(tuple(line.strip().split('/')))
-    return files
+            row = tuple(line.strip().split('/'))
+            if check_ext(ext, row):
+                files.append(row)
+    return files, ext
 
 
 def test_yield_files(expected_files):
     ll = len(ROOT.parts) - 1
+    ext = expected_files[1]
     files = ld.yield_files(ROOT,'*')
-    exp = expected_files
+    exp = expected_files[0]
     for file in files:
         ff = Path(file).parts[ll:]
         assert ff in exp
