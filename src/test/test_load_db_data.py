683d7243ae2b03a014727c2aac132027816508da
“Michal-D4” <mihal.d44@gmail.com>
2019-12-18 22:11:16 +0300

    test_insert_dir method

--- src/test/test_load_db_data.py
+++ src/test/test_load_db_data.py
@@ -18,6 +18,11 @@ logger.remove()
     '*'
 ])
 def expected_files(request):
+    """
+    List of files to be loaded/found with 'yield_files' function
+    @param request: list of file extensions
+    @return: expected_list_of_files, extensions_parameter_for_yield_files
+    """
     def check_ext(ext_, file_parts: tuple) -> bool:
         if '*' in ext_:
             return True
@@ -28,8 +33,8 @@ def expected_files(request):
 
     ext = request.param
     files = []
-    # 'file_list.txt' - file with expected results
-    with open(PATH_TO_DATA / 'file_list.txt') as fl:
+    expected = 'file_list.txt'
+    with open(PATH_TO_DATA / expected) as fl:
         for line in fl:
             row = tuple(line.strip().split('/'))
             if check_ext(ext, row):
@@ -49,8 +54,8 @@ def init_load() -> (ld.LoadDBData, sqlite3.Connection):
 
 @pytest.mark.parametrize('root, ext, expect',
                          [(ROOT / '.dir3', ('txt', 'py'), 2),
-                          (ROOT, '*', 11),
-                          (ROOT, '', 0),     # because files without extension are not loaded
+                          (ROOT, '*', 14),
+                          (ROOT, '', 3),
                           ])
 def test_load_data_file_count(init_load, root, ext, expect):
     """
@@ -72,8 +77,25 @@ def test_yield_files(expected_files):
         assert ff in exp
 
 
-def test_insert_dir():
-    pass
+def test_insert_dir(init_load, expected_files):
+    def create_dir(root_: Path, file_parts: tuple) -> Path:
+        rr = root_
+        for fp in file_parts:
+            rr = rr / fp
+        return rr.parent
+
+    load_d, conn_d = init_load
+    root = ROOT.parent
+    dirs = []
+    for files in expected_files:
+        for file in files:
+            dir_ = create_dir(root,  file)
+            idx, inserted = load_d.insert_dir(dir_)
+            if dir_ in dirs:
+                assert not inserted
+            else:
+                assert inserted
+                dirs.append(dir_)
 
 
 def test_search_closest_parent():
