52e69eaddb320db80c187ebcf601d4fac5cecf5f
“Michal-D4” <mihal.d44@gmail.com>
2019-12-19 21:48:32 +0300

    loguru shit - change level ONLY in code

--- src/__main__.py
+++ src/__main__.py
@@ -59,10 +59,10 @@ def main():
     fmt = '<green>{time:HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | ' \
           '<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> '   \
           '- <level>{message}</level>'
-    logger.add(sys.stderr, format=fmt, enqueue = True)
+    logger.add(sys.stderr, level="INFO", format=fmt, enqueue = True)
     # logger.add("cucu_{time:MMM-DD_HH-mm}.log", format=fmt, enqueue=True)
 
-    logger.debug("logger add")
+    logger.info("logger add")
 
     app = QApplication(sys.argv)
 
