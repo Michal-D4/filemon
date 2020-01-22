4a8aeb64c7568b414e69889666de60dc4af6321e
“Michal-D4” <mihal.d44@gmail.com>
2020-01-22 08:34:03 +0300

    on delete cascade

--- src/inter/method_tree.py
+++ src/inter/method_tree.py
@@ -10,7 +10,7 @@ DB = Path.cwd() / "prj.db"
 methods = (
     "create table IF NOT EXISTS methods ("
     "ID INTEGER NOT NULL PRIMARY KEY, "
-    "type text, "  # type - {m – method/function; sql; c – const; f – field; …}
+    "type text, "
     "module text, "
     "class text, "
     "method text, "
@@ -19,7 +19,7 @@ methods = (
     "c_method text, "
     "remark text);"
 )
-link_path = (
+link_path = (  #  redundant table ?
     "create table IF NOT EXISTS link_path ("
     "ID integer, "
     "call_ID integer, "
@@ -31,24 +31,22 @@ link_path = (
 )
 simple_link = (
     "create table IF NOT EXISTS simple_link ("
-    "ID integer, "
-    "call_ID integer, "
+    "ID integer references methods2(ID) ON DELETE CASCADE, "
+    "call_ID integer references methods2(ID) ON DELETE CASCADE, "
     "level integer, "
-    "primary key(ID, call_ID), "
-    "foreign key (ID) references methods2(ID), "
-    "foreign key (call_ID) references methods2(ID));"
+    "primary key(ID, call_ID));"
 )
 one_link = (
     "create table IF NOT EXISTS one_link ("
-    "ID integer, "
-    "call_ID integer, "
+    "ID integer references methods2(ID) ON DELETE CASCADE, "
+    "call_ID integer references methods2(ID) ON DELETE CASCADE, "
     "primary key(ID, call_ID));"
 )
 
 methods2 = (
     "create table IF NOT EXISTS methods2 ("
     "ID INTEGER NOT NULL PRIMARY KEY, "
-    "type text, "  # type - {m – method/function; sql; c – const; f – field; …}
+    "type text, "
     "module text, "
     "class text, "
     "method text, "
@@ -83,7 +81,7 @@ others = (
         "and methods.class = methods2.class "
         "and methods.method = methods2.method);"
     ),
-    (
+    (  # only first level links
         "insert into one_link (ID, call_ID) "
         "select a.ID, b.ID from methods c "
         "left join methods2 a on a.module = c.module "
@@ -92,15 +90,15 @@ others = (
         "and b.class = c.c_class and b.method = c.c_method "
         "where b.ID is not null;"
     ),
-    (
+    (  # all levels link with full path -- redundant
         "with recc (ID, call_ID, level, path) as ("
         "select ID, call_ID, 1, '' || call_ID || '/' || ID from one_link "
         "union select b.ID, a.call_ID, a.level+1, a.path || '/' || b.id "
         "from recc a join one_link b on b.call_ID = a.ID) "
         "insert into link_path (ID, call_ID, level, path) "
         "select * from recc where level > 1;"
-    ),
-    (
+    ), 
+    (  # all pairs link of min level without path
         "insert into simple_link (id, call_ID, level) "
         "select id, call_ID, min(level) from link_path "
         "group by id, call_ID;"
@@ -119,13 +117,14 @@ def drop_tables(con_):
 def create_tables(con_):
     con_.execute(methods)
     con_.execute(methods2)
+    print(one_link)
     con_.execute(one_link)
     con_.execute(link_path)
     con_.execute(simple_link)
 
 
 def recreate_tables(con_):
-    drop_tables(con_)
+    # drop_tables(con_)
     create_tables(con_)
 
 
@@ -157,9 +156,9 @@ if __name__ == "__main__":
     con_ = sqlite3.connect(DB)
 
     # either
-    clear_tables(con_)
+    # clear_tables(con_)
     # or
-    # recreate_tables(con_)
+    recreate_tables(con_)
 
     fill_methods(con_, in_file)
 
