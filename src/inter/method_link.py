b5828a0518dc1bda756f0639b1abe4aad7f4ceec
“Michal-D4” <mihal.d44@gmail.com>
2020-01-29 20:48:13 +0300

    convert set_tree_view method to function

--- src/inter/method_link.py
+++ src/inter/method_link.py
@@ -113,7 +113,7 @@ class Window(QWidget):
         self.proxyModel.setDynamicSortFilter(True)
 
         self.proxyView = QTreeView()
-        self.set_tree_view(self.proxyView)
+        set_tree_view(self.proxyView)
         self.proxyView.setModel(self.proxyModel)
         self.proxyView.customContextMenuRequested.connect(self.pop_menu)
 
@@ -125,7 +125,7 @@ class Window(QWidget):
         self.link_type = QComboBox()
 
         self.resView = QTreeView()
-        self.set_tree_view(self.resView)
+        set_tree_view(self.resView)
         self.resModel = QSortFilterProxyModel(self.resView)
         self.resView.setModel(self.resModel)
         self.resView.customContextMenuRequested.connect(self.menu_res_view)
@@ -147,14 +147,6 @@ class Window(QWidget):
 
         self.setWindowTitle("Custom Sort/Filter Model")
         self.resize(900, 750)
-
-    def set_tree_view(self, view: QTreeView):
-        view.setRootIsDecorated(False)
-        view.setAlternatingRowColors(True)
-        view.setSortingEnabled(True)
-        view.sortByColumn(1, Qt.AscendingOrder)
-        view.setContextMenuPolicy(Qt.CustomContextMenu)
-        view.setSelectionMode(QAbstractItemView.ExtendedSelection)
 
     def set_layout(self):
         filter_box: QGroupBox = self.set_filter_box()
@@ -717,6 +709,15 @@ def concrete_report(sort_key):
         for ll in lst:
             report.append((*pre, *ll, *post))
     return sorted_report
+
+
+def set_tree_view(view: QTreeView):
+    view.setRootIsDecorated(False)
+    view.setAlternatingRowColors(True)
+    view.setSortingEnabled(True)
+    view.sortByColumn(1, Qt.AscendingOrder)
+    view.setContextMenuPolicy(Qt.CustomContextMenu)
+    view.setSelectionMode(QAbstractItemView.ExtendedSelection)
 
 
 menu_items = (
