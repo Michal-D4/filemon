0c86ce5ea36a4ffa8cf45416ba429ffdcd810645
Michal-D4 <mihal.d44@gmail.com>
2020-12-29 22:34:22 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name="filemon", packages=find_packages())
+setup(
+    name="filemon", 
+    version='0.1.0',
+    license='MIT',
+    description='e-book management',
+
+    author='Mihas Davidovich',
+    author_email='mihal.d44@gmail.com',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'}
+
+    )
